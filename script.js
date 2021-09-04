27eced33c374e050aa89e4b06fefabce354535f2
subu00 <73189091+subu00@users.noreply.github.com>
2021-09-04 15:28:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const birthDate = document.querySelector('.date');
+const luckyNumber = document.querySelector('.ln');
+const buttonCheck = document.querySelector('.btn');
+const result = document.querySelector('.output');
+buttonCheck.addEventListener('click', clickHandler);
+
+function clickHandler() {
+    const dob = birthDate.value;
+    const sum = calculateSumOfBirthDate(dob);
+    if (sum && dob) {
+        birthDateIsLuckyOrNot(sum, luckyNumber.value);
+    } else {
+        result.innerText = 'Please enter the fields correctly.';
+    }
+}
+
+function calculateSumOfBirthDate(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+
+function birthDateIsLuckyOrNot(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        result.innerText = 'Congratulations your Birthday is lucky';
+    } else {
+        result.innerText = ' Oh, No!! Your Birthday is not lucky.';
+    }
+}
